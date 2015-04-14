cf247d826a1409405bc514fabfb0e1b261fb4ed4
Achint Verma <achint@mail.sfsu.edu>
2015-04-13 20:19:10 -0700

    added rating text and removed description

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -21,14 +21,14 @@
 					?>
 					<div class="col-md-4 drink-box">
 						<img src="{{URL::to('/')}}/images/drink-default.png" class="animated pulse">
-						<div class="progress" style="height:5px;">
+						<div class="progress" style="height:5px; background-color:#ccc;" title="Cocktail Rating: <?php echo $drink->rating; ?> / 10">
 						  <div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="<?php echo 10*($drink->rating); ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo 10*($drink->rating); ?>%">
 						    <span class="sr-only">40% Complete (success)</span>
 						  </div>
 						</div>
-						 
+
 						<h3><?php echo $drink->drink_name; ?></h3>
-						<p><?php echo substr($drink->description, 0, 100); ?> <?php if(strlen($drink->description) > 100) echo "...";?> </p>
+						<!-- <p><?php echo substr($drink->description, 0, 100); ?> <?php if(strlen($drink->description) > 100) echo "...";?> </p> -->
 
 						<span class="label label-small label-success">Orange Juice</span> 
 						<span class="label label-small label-success">Gin</span>
