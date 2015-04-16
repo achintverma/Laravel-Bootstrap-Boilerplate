f17e3cdc0059ce0b9fbdc9c5e8848c7dbf05745d
Achint Verma <achint@mail.sfsu.edu>
2015-04-15 17:16:18 -0700

    Added Ingredients Controller

---
+++ app/Ingredient.php
@@ -0,0 +1,11 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model {
+
+	// define the name of the table associated with this
+	protected $table  = "ingredients"; 
+
+
+}
--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -28,7 +28,7 @@
 						</div>
 
 						<h3><?php echo $drink->drink_name; ?></h3>
-						<!-- <p><?php echo substr($drink->description, 0, 100); ?> <?php if(strlen($drink->description) > 100) echo "...";?> </p> -->
+						<p><?php echo substr($drink->description, 0, 100); ?> <?php if(strlen($drink->description) > 100) echo "...";?> </p>
 
 						<span class="label label-small label-success">Orange Juice</span> 
 						<span class="label label-small label-success">Gin</span>
